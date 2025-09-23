44b9f9754c033705cf4a9a243c249541cf5a088a
Léo Milbor <20482270+leo-milbor@users.noreply.github.com>
2025-09-24 01:02:35 +0800

    feat: better theme handling

--- lib/main.dart
+++ lib/main.dart
@@ -46,11 +46,11 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       title: FuckYourChargesApp.title,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
       ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme.of(context).colorScheme.primary,
           title: Center(child: Text(FuckYourChargesApp.title)),
           leading: Builder(
             builder: (context) {
@@ -71,8 +71,10 @@ class _HomePageState extends State<HomePage> {
                 // Important: Remove any padding from the ListView.
                 padding: EdgeInsets.zero,
                 children: [
-                  const DrawerHeader(
-                    decoration: BoxDecoration(color: Colors.blue),
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
                     child: Text('Menu'),
                   ),
                   ListTile(
